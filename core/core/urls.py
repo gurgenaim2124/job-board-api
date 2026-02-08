56ea178c4ee028f0e40a9d2aec5d2059bb668bc9
gurgenaim2124 <Gurgena>
2026-02-09 01:52:30 +0400

    "Add Swagger (drf-yasg) documentation setup"

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,9 +17,30 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/users/', include('users.urls')),
+# ]
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Job Board API",
+      default_version='v1',
+      description="API documentation for Job Board project",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
